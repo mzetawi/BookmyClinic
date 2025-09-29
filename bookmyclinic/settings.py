229f01f973aeb4262d1606b747aec283c4b58021
Mustafa Zetawi <mustafa.m.zetawi@gmail.com>
2025-09-29 12:08:39 +0300

    Update settings.py

--- bookmyclinic/settings.py
+++ bookmyclinic/settings.py
@@ -76,16 +76,8 @@ WSGI_APPLICATION = 'bookmyclinic.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookmyclinic',       
-        'USER': 'clinic_user',        
-        'PASSWORD': 'ClinicPass123!', 
-        'HOST': 'localhost',          
-        'PORT': '3306',               
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
